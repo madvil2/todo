d405921708544ef2d6a35ca74a6688bb0f56020f
madvil2 <KAKaimov@sberbank.ru>
2020-09-14 16:58:47 +0300

    Updates

---
+++ precache-manifest.3a2913539d09299e1f5691041e392421.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75640951709a56abc3a161c776fb7b19",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "b0c65abb7a30d774db60",
+    "url": "/todo/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "1d7a4b4a8cc40098ab0f",
+    "url": "/todo/static/css/main.56094253.chunk.css"
+  },
+  {
+    "revision": "b0c65abb7a30d774db60",
+    "url": "/todo/static/js/2.a3117b10.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/todo/static/js/2.a3117b10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d7a4b4a8cc40098ab0f",
+    "url": "/todo/static/js/main.e5559f86.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
